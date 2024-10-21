cf917d0e3de239e120edf7a885ee4f7daeb5bf80
Pham The Tinh <phamthetinhcop1312@gmail.com>
2024-10-21 20:35:04 +0700

    features fe reportPayment export pdf

--- src/App.js
+++ src/App.js
@@ -1,69 +1,76 @@
-import './App.css';
+import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import AdminLayOut from './Component/View/Admin/AdminLayOut';
-import OrderPage from './Component/View/Admin/Order/OrderPage';
-import HomeLayOut from './Component/View/Home/HomeLayOut';
-import UserPage from './Component/View/Admin/User/UserPage';
-import Header from './Component/Include/Home/Header';
-import FoodPage from './Component/View/Admin/Product/FoodPage';
-import FoodIndex from './Component/View/Home/Index/FoodIndex';
-import Order from './Component/View/Home/Details/Order';
-import FoodDetails from './Component/View/Home/Details/FoodDetails';
-import Footer from './Component/Include/Home/Footer';
-import CartPage from './Component/View/Home/Cart/CartPage';
-import Thanks from './Component/View/Home/Thank/Thanks';
-import BoxChatPage from './Component/View/Home/Chat/BoxChatPage';
-import CouponPage from './Component/View/Admin/Coupon/CouponPage';
-import CouponFormPage from './Component/View/Admin/Coupon/CouponFormPage';
-import CouponStoragePage from './Component/View/Home/CouponStorage/CouponStoragePage';
-import ClaimCoupon from './Component/View/Home/CouponStorage/ClaimCoupon';
-import ClaimCouponPage from './Component/View/Home/CouponStorage/ClaimCouponPage';
-import OrderHistoryPage from './Component/View/Home/OrderHistory/OrderHistoryPage';
-import Login from './Component/View/Home/Login/Login';
-import TableReservation from './Component/View/Home/TableReservation/TableReservation';
-import ReservationList from './Component/View/Admin/Reservation/ReservationList';
-import ReservationCancelledList from './Component/View/Admin/Reservation/ReservationCancelledList';
-import ReservationAcceptedList from './Component/View/Admin/Reservation/ReservationAcceptedList';
-import OrderStatus from './Component/View/Home/MyOrder/OrderStatus';
-import Notification from './Component/View/Admin/Notification/Notification';
-import Revenue from './Component/View/Admin/Revenue/Revenue';
-import Inventory from './Component/View/Admin/Inventory/Inventory.js';
-import UserOrder from './Component/View/Admin/UserBuy/UserOrder.js'
-import UserFormPage from './Component/View/Admin/User/UserFormPage.js';
-import WishList from './Component/View/Home/WishList/WishList.js';
+import AdminLayOut from "./Component/View/Admin/AdminLayOut";
+import OrderPage from "./Component/View/Admin/Order/OrderPage";
+import HomeLayOut from "./Component/View/Home/HomeLayOut";
+import UserPage from "./Component/View/Admin/User/UserPage";
+import Header from "./Component/Include/Home/Header";
+import FoodPage from "./Component/View/Admin/Product/FoodPage";
+import FoodIndex from "./Component/View/Home/Index/FoodIndex";
+import Order from "./Component/View/Home/Details/Order";
+import FoodDetails from "./Component/View/Home/Details/FoodDetails";
+import Footer from "./Component/Include/Home/Footer";
+import CartPage from "./Component/View/Home/Cart/CartPage";
+import Thanks from "./Component/View/Home/Thank/Thanks";
+import BoxChatPage from "./Component/View/Home/Chat/BoxChatPage";
+import CouponPage from "./Component/View/Admin/Coupon/CouponPage";
+import CouponFormPage from "./Component/View/Admin/Coupon/CouponFormPage";
+import CouponStoragePage from "./Component/View/Home/CouponStorage/CouponStoragePage";
+import ClaimCoupon from "./Component/View/Home/CouponStorage/ClaimCoupon";
+import ClaimCouponPage from "./Component/View/Home/CouponStorage/ClaimCouponPage";
+import OrderHistoryPage from "./Component/View/Home/OrderHistory/OrderHistoryPage";
+import Login from "./Component/View/Home/Login/Login";
+import TableReservation from "./Component/View/Home/TableReservation/TableReservation";
+import ReservationList from "./Component/View/Admin/Reservation/ReservationList";
+import ReservationCancelledList from "./Component/View/Admin/Reservation/ReservationCancelledList";
+import ReservationAcceptedList from "./Component/View/Admin/Reservation/ReservationAcceptedList";
+import OrderStatus from "./Component/View/Home/MyOrder/OrderStatus";
+import Notification from "./Component/View/Admin/Notification/Notification";
+import Revenue from "./Component/View/Admin/Revenue/Revenue";
+import Inventory from "./Component/View/Admin/Inventory/Inventory.js";
+import UserOrder from "./Component/View/Admin/UserBuy/UserOrder.js";
+import UserFormPage from "./Component/View/Admin/User/UserFormPage.js";
+import WishList from "./Component/View/Home/WishList/WishList.js";
+import PaymentReport from "./Component/View/Admin/PaymentReport/PaymentReport.js";
 
 function App() {
   return (
-    <div className="App"> 
-     <BrowserRouter>
-     <Header />
+    <div className="App">
+      <BrowserRouter>
+        <Header />
         <Routes>
           <Route path="/" element={<HomeLayOut />}>
             <Route index element={<FoodIndex />} />
-            <Route path='login' element={<Login />} />
-            <Route path='notification' element={<Notification/>}/>
+            <Route path="login" element={<Login />} />
+            <Route path="notification" element={<Notification />} />
             <Route path="foodDetailsPopup" element={<Order />} />
-            <Route path='foodDetails/:id' element={<FoodDetails />} />
-            <Route path='cart/:cartId' element={<CartPage/>} />
-            <Route path='/thanks/:paymentmethod' element={<Thanks />} />
-            <Route path='chat' element={<BoxChatPage/>} />
-            <Route path='couponStorage' element={<CouponStoragePage />} />
+            <Route path="foodDetails/:id" element={<FoodDetails />} />
+            <Route path="cart/:cartId" element={<CartPage />} />
+            <Route path="/thanks/:paymentmethod" element={<Thanks />} />
+            <Route path="chat" element={<BoxChatPage />} />
+            <Route path="couponStorage" element={<CouponStoragePage />} />
             <Route path="WishList" element={<WishList />} />
-            <Route path='claimCoupon' element={<ClaimCouponPage />} />
-            <Route path='orderHistory/order/:userName' element={<OrderHistoryPage />} />
+            <Route path="claimCoupon" element={<ClaimCouponPage />} />
+            <Route
+              path="orderHistory/order/:userName"
+              element={<OrderHistoryPage />}
+            />
             <Route path="table-reservation" element={<TableReservation />} />
-            <Route path='/myOrder' element={<OrderStatus/>} />
+            <Route path="/myOrder" element={<OrderStatus />} />
           </Route>
 
           <Route path="/admin" element={<AdminLayOut />}>
             <Route index element={<OrderPage />} />
-            <Route path="users" element ={<UserPage />} />
+            <Route path="users" element={<UserPage />} />
             <Route path="user/create" element={<UserFormPage />} />
             <Route path="user/edit/:userId" element={<UserFormPage />} />
             <Route path="foods" element={<FoodPage />} />
             <Route path="coupons" element={<CouponPage />} />
             <Route path="coupon/create" element={<CouponFormPage />} />
-            <Route path="coupon/update/:couponId" element= {<CouponFormPage />} />
+            <Route
+              path="coupon/update/:couponId"
+              element={<CouponFormPage />}
+            />
             <Route path="reservation-list" element={<ReservationList />} />
             <Route
               path="reservation-cancelled-list"
@@ -74,12 +81,13 @@ function App() {
               element={<ReservationAcceptedList />}
             />
             <Route path="revenue" element={<Revenue />} />
-          <Route path="inventory" element={<Inventory />} />
-          <Route path="UserOrder" element={<UserOrder />} />
+            <Route path="reportpayment" element={<PaymentReport />} />
+            <Route path="inventory" element={<Inventory />} />
+            <Route path="UserOrder" element={<UserOrder />} />
           </Route>
         </Routes>
         <Footer />
-     </BrowserRouter>
+      </BrowserRouter>
     </div>
   );
 }
--- src/Component/View/Admin/Order/OrderList.js
+++ src/Component/View/Admin/Order/OrderList.js
@@ -1,164 +1,197 @@
-import React,{useState,useEffect} from 'react';
-import Modal from './Modal';
-import axiosConfig from '../../../Config/AxiosConfig';
-import ReturnRequestPopup from '../OrderReturn/ReturnRequestPopup';
-import CustomAlert from '../../../Config/CustomAlert';
+import React, { useState, useEffect } from "react";
+import Modal from "./Modal";
+import axiosConfig from "../../../Config/AxiosConfig";
+import ReturnRequestPopup from "../OrderReturn/ReturnRequestPopup";
+import CustomAlert from "../../../Config/CustomAlert";
+import InvoiceDownloadComponent from "./InvoiceDownloadComponent";
 const OrderList = () => {
-  const [order,setOrder]=useState([]);
-  const [orderDetails, setOrderDetails] = useState([]); 
+  const [order, setOrder] = useState([]);
+  const [orderDetails, setOrderDetails] = useState([]);
   const [selectedItem, setSelectedItem] = useState(null);
-  const [isOpenReturnOrder,setIsOpentReturnOrder] = useState(null);
+  const [isOpenReturnOrder, setIsOpentReturnOrder] = useState(null);
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [orderReturnByOrderId,setOrderReturnByOrderId] = useState([]);
-  const [alert,setAlert] = useState(null);
-const featchOrderList = async ()=>{
-  try {
-    const responseOrder = await axiosConfig('/order/findAll')
-    .then(responseOrder =>{
-      setOrder(responseOrder.data)
-      console.log(responseOrder.data)
-    })
-  } catch (error) {
-    console.log(error,'Lỗi nhận dử liệu order')
-  }
-}
-
-
-const handleInfoClick = (item,order) => {
-  setSelectedItem(item);
-  setOrderDetails(order);
-  setIsModalOpen(true);
-};
-
-const handleCloseModal = () => {
-  setIsModalOpen(false);
-  setSelectedItem(null);
-};
-const checkOrderReturnRequest =async (orderId) => {
-  console.log(orderId);
-  setIsOpentReturnOrder(!isOpenReturnOrder);
-  try {
-    const resOrderReturnByOrderId = await axiosConfig.get(`/orderReturn/byOrderId/${orderId}`);
-    console.log(resOrderReturnByOrderId.data.data);
-    setOrderReturnByOrderId(resOrderReturnByOrderId.data.data);
-  } catch (error) {
-    console.error('error in checkOrderReturnRequest',error);
-  }  
-}
-
-const hanldeApproveOrderReturn = async(orderId,isApprove) => {
-  const formData = new FormData();
-  formData.append("isApprove",new Blob([JSON.stringify(isApprove)], {type : 'application/json'}));
+  const [orderReturnByOrderId, setOrderReturnByOrderId] = useState([]);
+  const [alert, setAlert] = useState(null);
+  const featchOrderList = async () => {
     try {
-      const resOrderReturnApprove = await axiosConfig.put(`/orderReturn/isApprove/${orderId}`,formData);
+      const responseOrder = await axiosConfig("/order/findAll").then(
+        (responseOrder) => {
+          setOrder(responseOrder.data);
+          console.log(responseOrder.data);
+        }
+      );
+    } catch (error) {
+      console.log(error, "Lỗi nhận dử liệu order");
+    }
+  };
+
+  const handleInfoClick = (item, order) => {
+    setSelectedItem(item);
+    setOrderDetails(order);
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+    setSelectedItem(null);
+  };
+  const checkOrderReturnRequest = async (orderId) => {
+    console.log(orderId);
+    setIsOpentReturnOrder(!isOpenReturnOrder);
+    try {
+      const resOrderReturnByOrderId = await axiosConfig.get(
+        `/orderReturn/byOrderId/${orderId}`
+      );
+      console.log(resOrderReturnByOrderId.data.data);
+      setOrderReturnByOrderId(resOrderReturnByOrderId.data.data);
+    } catch (error) {
+      console.error("error in checkOrderReturnRequest", error);
+    }
+  };
+
+  const hanldeApproveOrderReturn = async (orderId, isApprove) => {
+    const formData = new FormData();
+    formData.append(
+      "isApprove",
+      new Blob([JSON.stringify(isApprove)], { type: "application/json" })
+    );
+    try {
+      const resOrderReturnApprove = await axiosConfig.put(
+        `/orderReturn/isApprove/${orderId}`,
+        formData
+      );
       console.log(resOrderReturnApprove.data);
-      if(isApprove === true &&resOrderReturnApprove.data.data !== null){
-        setAlert({type : 'success' , message : 'Accept refund request! '});
-      }else if(isApprove === false &&resOrderReturnApprove.data.data !== null){
-        setAlert({type : 'error' , message : 'Refund request denied'})
+      if (isApprove === true && resOrderReturnApprove.data.data !== null) {
+        setAlert({ type: "success", message: "Accept refund request! " });
+      } else if (
+        isApprove === false &&
+        resOrderReturnApprove.data.data !== null
+      ) {
+        setAlert({ type: "error", message: "Refund request denied" });
       }
       setIsOpentReturnOrder(!isOpenReturnOrder);
       featchOrderList();
     } catch (error) {
-      console.error('error in handle Approve OrderReturn',error);
+      console.error("error in handle Approve OrderReturn", error);
     }
-}
+  };
 
-
- useEffect (()=>{
-  featchOrderList();
-},[]);
-    return (
-      <div className="body " >
-     {
-      alert && (
-        <CustomAlert 
-      type={alert.type}
-      message={alert.message}
-      onClose={() => setAlert(null)}
-      />
-      )
-     }
-        <div id="reportsPage">
-      <div id="home">
-        <div className="container">
-          <div className="row tm-content-row">
-           
-           
-
-            <div className="col-12 tm-block-col">
-              <div className=" ">
-                <h2 className="tm-block-title">Orders List</h2>
-                <table className="table">
-                  <thead>
-                    <tr>
-                    <th>STT</th>
-                      <th scope="col">ORDER NO.</th>
-                      <th scope="col">Order Date</th>
-                      <th scope="col">Order Time</th>
-                      <th scope="col">User Name</th>
-                      <th scope="col">Delivery Address</th>
-                      <th scope="col">	Order Status</th>
-                      <th scope="col">Payment Method</th>
-                      <th scope="col">Ship Method</th>
-                      <th scope="col">Total Price</th>
-                      <th scope="col">Total Quantity</th>
-                      <th scope="col">Funtion</th>
-                      <th></th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                  {order.map((item,index)=>(
-                        <tr key={index}>
-                        <td>{index}</td>
-                        <th scope="row">#{item.orderId}</th>
-                        <td>  {(() => {
-                      const orderDate = new Date(item.orderDate);
-                      return `${orderDate.getFullYear()}/${String(orderDate.getMonth() + 1).padStart(2, '0')}/${String(orderDate.getDate()).padStart(2, '0')}`;
-                  })()}</td>
-                      
-                        <td><b>{item.orderTime}</b></td>
-                        <td><b>{item.user.fullName}</b></td>
-                        <td><b>{item.deliveryAddress}</b></td>
-                        <td>{item.orderStatus.orderStatusName}</td>
-                        <td>{item.paymentMethod.paymentName}</td>
-                        <td>{item.shipMethod.shipName}</td>
-                        <td>{item.totalPrice.toLocaleString('vi-VN')}đ</td>              
-                        <td>{item.totalQuantity}</td>
-                        <td onClick={() => handleInfoClick(item.orderId,item)}>
-                        <i className="fa-solid fa-circle-info fa-lg"></i>
-                        </td>
-                        <td> {item.orderStatus.orderStatusName === 'Return Requested' && (
-                                <button style={{fontSize : '10px'}}  onClick={() => checkOrderReturnRequest(item.orderId)}>Check</button>
-                        )}</td>
-                      </tr>
-                  ))
-                    }
-                  
-                   
-                  </tbody>
-                </table>
-                {isModalOpen && (
-        <Modal
-          item={selectedItem}
-          order={orderDetails}
-          onClose={handleCloseModal}
+  useEffect(() => {
+    featchOrderList();
+  }, []);
+  return (
+    <div className="body ">
+      {alert && (
+        <CustomAlert
+          type={alert.type}
+          message={alert.message}
+          onClose={() => setAlert(null)}
         />
       )}
-      <ReturnRequestPopup
-      isOpenReturnOrder={isOpenReturnOrder}
-      checkOrderReturnRequest={checkOrderReturnRequest}
-      orderReturnByOrderId={orderReturnByOrderId}
-      hanldeApproveOrderReturn = {hanldeApproveOrderReturn}
-       />
+      <div id="reportsPage">
+        <div id="home">
+          <div className="container">
+            <div className="row tm-content-row">
+              <div className="col-12 tm-block-col">
+                <div className=" ">
+                  <h2 className="tm-block-title">Orders List</h2>
+                  <table className="table">
+                    <thead>
+                      <tr>
+                        <th>STT</th>
+                        <th scope="col">ORDER NO.</th>
+                        <th scope="col">Order Date</th>
+                        <th scope="col">Order Time</th>
+                        <th scope="col">User Name</th>
+                        <th scope="col">Delivery Address</th>
+                        <th scope="col"> Order Status</th>
+                        <th scope="col">Payment Method</th>
+                        <th scope="col">Ship Method</th>
+                        <th scope="col">Total Price</th>
+                        <th scope="col">Total Quantity</th>
+                        <th scope="col">Funtion</th>
+                        <th></th>
+                        <th></th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {order.map((item, index) => (
+                        <tr key={index}>
+                          <td>{index}</td>
+                          <th scope="row">#{item.orderId}</th>
+                          <td>
+                            {" "}
+                            {(() => {
+                              const orderDate = new Date(item.orderDate);
+                              return `${orderDate.getFullYear()}/${String(
+                                orderDate.getMonth() + 1
+                              ).padStart(2, "0")}/${String(
+                                orderDate.getDate()
+                              ).padStart(2, "0")}`;
+                            })()}
+                          </td>
+
+                          <td>
+                            <b>{item.orderTime}</b>
+                          </td>
+                          <td>
+                            <b>{item.user.fullName}</b>
+                          </td>
+                          <td>
+                            <b>{item.deliveryAddress}</b>
+                          </td>
+                          <td>{item.orderStatus.orderStatusName}</td>
+                          <td>{item.paymentMethod.paymentName}</td>
+                          <td>{item.shipMethod.shipName}</td>
+                          <td>{item.totalPrice.toLocaleString("vi-VN")}đ</td>
+                          <td>{item.totalQuantity}</td>
+                          <td
+                            onClick={() => handleInfoClick(item.orderId, item)}
+                          >
+                            <i className="fa-solid fa-circle-info fa-lg"></i>
+                          </td>
+                          <td>
+                            {" "}
+                            {item.orderStatus.orderStatusName ===
+                              "Return Requested" && (
+                              <button
+                                style={{ fontSize: "10px" }}
+                                onClick={() =>
+                                  checkOrderReturnRequest(item.orderId)
+                                }
+                              >
+                                Check
+                              </button>
+                            )}
+                          </td>
+                          <td>
+                            <InvoiceDownloadComponent orderId={item.orderId} />
+                          </td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>
+                  {isModalOpen && (
+                    <Modal
+                      item={selectedItem}
+                      order={orderDetails}
+                      onClose={handleCloseModal}
+                    />
+                  )}
+                  <ReturnRequestPopup
+                    isOpenReturnOrder={isOpenReturnOrder}
+                    checkOrderReturnRequest={checkOrderReturnRequest}
+                    orderReturnByOrderId={orderReturnByOrderId}
+                    hanldeApproveOrderReturn={hanldeApproveOrderReturn}
+                  />
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
     </div>
-    </div>
-    );
+  );
 };
 
 export default OrderList;
